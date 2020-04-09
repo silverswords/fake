3a2bd7a69d93f04f9bc8c7f7a8f427bb4db05e5c
YMH <1256815659@qq.com>
2020-04-09 22:48:58 +0800

    update
    fix id

--- app/infogenerate/model/model.go
+++ app/infogenerate/model/model.go
@@ -2,7 +2,7 @@ package model
 
 //InfoStruct is the information struct
 type InfoStruct struct {
-	ID    int    `json:"id,omitempty"`
+	ID    int    `json:"id"`
 	Name  string `json:"name,omitempty"`
 	Phone string `json:"phone,omitempty"`
 	Email string `json:"email,omitempty"`
